a59f25b9214f9415724b842b0ce725bb4a3884a4
Vitalik-Hakim <jupiterxxx11@gmail.com>
2022-07-24 12:25:38 +0000

    File structure

---
+++ uploads/my_exe_ticket_1880_220724_122406.py
@@ -0,0 +1,28 @@
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+mail_content = "Hello,"
+#The mail addresses and password
+sender_address = 'vitalikhakim@gmail.com'
+sender_pass = ''
+receiver_address = 'vitalikhakim@gmail'
+#Setup the MIME
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = receiver_address
+message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
+#The body and the attachments for the mail
+message.attach(MIMEText(mail_content, 'plain'))
+#Create SMTP session for sending the mail
+session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+session.starttls() #enable security
+session.login(sender_address, sender_pass) #login with mail_id and password
+text = message.as_string()
+session.sendmail(sender_address, receiver_address, text)
+session.quit()
+print('Mail Sent')
+
+
+
